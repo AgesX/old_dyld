60211530c09a8519251ed313d99ad41e2634a3c8
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-05 15:29:48 +0800

    question   +

---
+++ face/proj/ppp/ppp/main.m
@@ -0,0 +1,28 @@
+//
+//  main.m
+//  ppp
+//
+//  Created by Jz D on 2021/8/5.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+#import <objc/runtime.h>
+
+#import "LGPerson.h"
+#import "LGTeacher.h"
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        LGTeacher *teacher = [[LGTeacher alloc] init];;
+    }
+    return 0;
+}
