1a5d5d43e777d68a7782782cf01c8380297c389c
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-05 18:03:27 +0800

    //  v@:         //  3 个符号，代表，3 个参数

--- face/proj/runtime/runtime/other/LGStudent.m
+++ face/proj/runtime/runtime/other/LGStudent.m
@@ -10,6 +10,70 @@
 #import "LGDog.h"
 
 @implementation LGStudent
+
+
+
+
+
+void lg_dynamicMethodIMP(){
+    NSLog(@"%s",__func__);
+}
+// 网络 数据 - int string
+// [int length] --> 做一些处理 -
+// 写代码 在你奔溃 之前 - 动态处理
+// crash 收集 - 动态方法解析
+// 也不好 --
+
+
+
+
++ (BOOL)resolveInstanceMethod:(SEL)sel{
+    // 请你处理 - 给第一次机会
+    // sel - imp
+    NSLog(@"来了,老弟");
+    // load - 可以
+    // 方法 -- 动态化
+    // lg_run  --
+    if (sel==@selector(run)) {
+        class_addMethod([self class], sel, (IMP)lg_dynamicMethodIMP, "v@:");
+        
+        
+        
+        
+        //  v@:
+        //  3 个符号，代表，3 个参数
+        //  v , void
+        //  @ , id   , target
+        //  : , selector, _cmd
+        
+        
+        return YES;
+    }
+    return NO;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ 
+ 
+ 
+ 
+ 
 //
 //- (id)forwardingTargetForSelector:(SEL)aSelector{
 //    NSLog(@"%s",__func__);
@@ -43,6 +107,13 @@
     anInvocation.selector = @selector(runMethod:);
     [anInvocation invoke];
 }
+
+ 
+ 
+ 
+ */
+
+
 
 
 @end
