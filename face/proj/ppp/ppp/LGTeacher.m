24037a68ceeafab7b8a995225b991d49119b78c3
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-06 10:29:02 +0800

    // 发送消息， 关键是看，消息的接受者         // super,  不是消息的接受者，         // super， 都不是对象

--- face/proj/ppp/ppp/LGTeacher.m
+++ face/proj/ppp/ppp/LGTeacher.m
@@ -8,22 +8,62 @@
 #import "LGTeacher.h"
 
 @implementation LGTeacher
-- (instancetype)init{
+- (instancetype)init{  // self , 可以算是，初始化函数的隐藏参数
+    
+    
+    
     self = [super init];
     if (self) {
         NSLog(@"%@ - %@",[self class],[super class]);
+        
+        
+        // 发送消息， 关键是看，消息的接受者
+        // super,  不是消息的接受者，
+        // super， 都不是对象
+        
     }
     return self;
+    
+    
+    
+    
 }
 
 // msgSend :
 // LGTeacher
 
+
+
+
+
+
+
+
 // super : 关键字
+
+
+
+
+
+
+
+
+
+
+
+
 // [super class] (class)(id self , sel _cmd)
+
+
 // self->isa LGTeacher
+
+
 // self 消息的接受者 LGTeacher
+
+
 // objc_msgSendSuper2  结构体
+
+
 // 没有分析对
 
 @end
